091f1888014778b6dc70266e671c5c9f5adf19a3
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2026-01-05 07:35:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('test');
+const checkboxes = document.querySelectorAll(".checklist input[type='checkbox']");
+const counter = document.getElementById("counter");
+
+function updateCounter() {
+  const checked = document.querySelectorAll(
+    ".checklist input[type='checkbox']:checked"
+  ).length;
+
+  const total = checkboxes.length;
+  counter.textContent = `${checked} / ${total} completed`;
+}
+
+checkboxes.forEach(cb => {
+  cb.addEventListener("change", updateCounter);
+});
+
+updateCounter();
